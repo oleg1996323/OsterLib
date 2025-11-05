74d24f1f1c8d9c5d4567928da6a01e790c1efdc2
oster <oleg.ternikov@gmail.com>
2025-11-05 08:29:47 +0300

    ZIP-compressor updated

--- include/compressor.h
+++ include/compressor.h
@@ -7,42 +7,88 @@ namespace fs = std::filesystem;
 
 namespace cpp::zip_ns{
     class Compressor{
+        enum ZIP_FLAGS{
+            //Perform additional stricter consistency checks on the archive, and error if they fail.
+            CHECK_CONSISTENCY = ZIP_CHECKCONS,
+            //Create the archive if it does not exist.
+            CREATE = ZIP_CREATE,
+            //Error if archive already exists.
+            CHECK_EXISTS = ZIP_EXCL,
+            //If archive exists, ignore its current contents. In other words, handle it the same way as an empty archive.
+            TRUNCATE = ZIP_TRUNCATE,
+            //Open archive in read-only mode.
+            READ_ONLY = ZIP_RDONLY
+        };
+
+        Compressor(const fs::path& zip_path, ZIP_FLAGS flags):path_(zip_path){
+            int error_code = 0;
+            zip_ = zip_open(zip_path.c_str(),flags,&error_code);
+            if(!zip_){
+                zip_error error_at_open;
+                zip_error_init_with_code(&error_at_open,error_code);
+                throw std::runtime_error(zip_error_strerror(&error_at_open));
+                return;
+            }
+        }
+
+        Compressor(const fs::path& zip_dir, const std::string& zip_name, ZIP_FLAGS flags):path_(zip_dir/zip_name){
+            int error_code = 0;
+            zip_ = zip_open(path_.c_str(),flags,&error_code);
+            if(!zip_){
+                zip_error error_at_open;
+                zip_error_init_with_code(&error_at_open,error_code);
+                throw std::runtime_error(zip_error_strerror(&error_at_open));
+                return;
+            }
+        }
         public:
-        Compressor(const fs::path& zip_path):path_(zip_path){
-            zip_ = zip_open(zip_path.c_str(),ZIP_CREATE|ZIP_TRUNCATE,&zip_err_code_);
-            if(zip_err_code_){
-                zip_error error_at_open;
-                zip_error_init_with_code(&error_at_open,zip_err_code_);
-                throw std::runtime_error(zip_error_strerror(&error_at_open));
-                return;
-            }
+        static Compressor create_archive(const fs::path& zip_dir, const std::string& zip_name){
+            return Compressor(zip_dir,zip_name,static_cast<ZIP_FLAGS>(ZIP_FLAGS::CREATE|ZIP_FLAGS::TRUNCATE));
+        }
+        static Compressor create_archive(const fs::path& zip_path){
+            return Compressor(zip_path,static_cast<ZIP_FLAGS>(ZIP_FLAGS::CREATE|ZIP_FLAGS::TRUNCATE));
         }
 
-        Compressor(const fs::path& zip_dir, const std::string& zip_name):path_(zip_dir/zip_name){
-            zip_ = zip_open(path_.c_str(),ZIP_CREATE|ZIP_TRUNCATE,&zip_err_code_);
-            if(zip_err_code_){
-                zip_error error_at_open;
-                zip_error_init_with_code(&error_at_open,zip_err_code_);
-                throw std::runtime_error(zip_error_strerror(&error_at_open));
-                return;
-            }
+        static Compressor open_archive(const fs::path& zip_dir, const std::string& zip_name){
+            return Compressor(zip_dir,zip_name,static_cast<ZIP_FLAGS>(0));
+        }
+        static Compressor open_archive(const fs::path& zip_path){
+            return Compressor(zip_path,static_cast<ZIP_FLAGS>(0));
         }
 
-        void add_file(const fs::path& rel_dir, const fs::path& file_path){
+        static Compressor open_archive_read_only(const fs::path& zip_dir, const std::string& zip_name){
+            return Compressor(zip_dir,zip_name,static_cast<ZIP_FLAGS>(ZIP_FLAGS::READ_ONLY));
+        }
+        static Compressor open_archive_read_only(const fs::path& zip_path){
+            return Compressor(zip_path,static_cast<ZIP_FLAGS>(ZIP_FLAGS::READ_ONLY));
+        }
+
+        bool add_file(const fs::path& rel_path, const fs::path& file_path) noexcept{
             if(zip_!=NULL){
-                zip_source* zs = zip_source_file(zip_,file_path.string().c_str(),0,0);
+                zip_source* zs = zip_source_file_create(file_path.c_str(),0,0,&err_);
                 if(!zs){
-                    std::cout<<"Unable to create source of file. Abort."<<std::endl;
-                    exit(1);
+                    std::cout<<zip_error_strerror(&err_)<<std::endl;
+                    return false;
                 }
-                if(zip_file_add(zip_,fs::relative(file_path,rel_dir).string().c_str(), zs,ZIP_FL_ENC_UTF_8)<0){
-                    std::cerr << "Failed to add file to archive: " << file_path << std::endl;
+                if(zip_file_add(zip_,fs::relative(file_path,rel_path).c_str(), zs,ZIP_FL_ENC_UTF_8)<0){
+                    std::cout<<zip_error_strerror(&err_)<<std::endl;
                     zip_source_free(zs);
-                    exit(1);
+                    return false;
                 }
+                else return true;
             }
+            else return false;
         }
 
+        static bool add_file(const fs::path& archive_path,const fs::path& rel_path, const fs::path& file_path) noexcept{
+            try{
+                return open_archive(archive_path).add_file(rel_path,file_path);
+            }
+            catch(const std::exception& err){
+                return false;
+            }
+        }
+        
         const fs::path& get_zip_path() const{
             return path_;
         }
@@ -55,6 +101,6 @@ namespace cpp::zip_ns{
         private:
         const fs::path& path_;
         zip* zip_ = nullptr;
-        int zip_err_code_ = 0;
+        zip_error_t err_;
     };
 }
